24edff3b95a2a9ce4ccea27d7a4692f089ba45c4
RahulThalwal <rahul835090@gmail.com>
2024-02-11 22:22:18 +0530

    first-commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import  product1 from "./assets/1.png";
+import  product2 from "./assets/2.png";
+import  product3 from "./assets/3.png";
+import  product4 from "./assets/4.png";
+import  product5 from "./assets/5.png";
+import  product6 from "./assets/6.png";
+import  product7 from "./assets/7.png";
+import  product8 from "./assets/8.png";
+
+export const PRODUCTS = [
+   {
+       id: 1,
+       productName: "IPHONE",
+       price: 999.0,
+       productImage: product1,
+   },
+   {
+       id: 2,
+       productName: "Macbook Pro 2024(M1)",
+       price: 1999.0,
+       productImage: product2,
+   },
+   {
+       id: 3,
+       productName: "GoPro HERO10 Black Bones",
+       price: 899.0,
+       productImage: product3,
+   },
+   {
+       id: 4,
+       productName: "Nike Air Force 1",
+       price: 599.0,
+       productImage: product4,
+   },
+   {
+       id: 5,
+       productName: " H&M Hoodie",
+       price: 199.0,
+       productImage: product5,
+   },
+   {
+       id: 6,
+       productName: "Men's Shirt",
+       price: 299.0,
+       productImage: product6,
+   },
+   {
+       id: 7,
+       productName: "Versatile Jacket",
+       price: 199.0,
+       productImage: product7,
+   },
+   {
+       id: 8,
+       productName: "INike Hoodie",
+       price: 399.0,
+       productImage: product8,
+   },
+]
